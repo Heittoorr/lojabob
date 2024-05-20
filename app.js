034e1155697ed04e9f2f31951170fd82b37d3c4b
Heittoorr <167815429+Heittoorr@users.noreply.github.com>
2024-05-20 17:24:05 -0300

    app.js

--- app.js
+++ app.js
@@ -1,25 +1,54 @@
 const mysql = require('mysql2')
-
+ 
 const connection = mysql.createConnection({
     host:'sql10.freemysqlhosting.net',
     user:'sql10701770',
     password:'uh4Vi5Y7dZ',
     database:'sql10701770'
+ 
 })
-
+ 
 connection.connect()
-
+ 
 //inserir dados - insert into nometabela
 function inserir(lg,sn,mail) {
-    let sql = "insert into users set ?"
+    let sql = "insert into users set?"
     let dados = {login:lg, senha:sn, email:mail}
-
-    connection.query(sql,dados, function(error, results,fields){
+ 
+    connection.query(sql,dados, function(error,results,fields){
         if(error) throw error
-        console.log("Cadastro com Sucesso!")
+        console.log("Cadastro com sucesso!")
     })
 }
-
-inserir("Heitor","12345","heitor@gmail.com")
-
+//selecionar dados por id - select nometabela
+function selecionaPorId(id){
+    let sql = "select * from users where id = ?"
+    connection.query(sql, id, function(error, results, fields){
+        for(let i=0;i<results.length;i++){
+        if(error) throw error
+        console.log("Selecionado: " + results[i].id + ":" + results[i].login + ":" + results[i].email)
+        }
+    })
+ 
+}
+ 
+function selecionarTudo(){
+    let sql = "select * from users"
+    connection.query(sql, function(error, results, fields){
+        for(let i=0;i<results.length;i++){
+        if(error) throw error
+        console.log("Selecionado: " + results[i].id + ":" + results[i].login + ":" + results[i].email)
+        }
+    })
+ 
+}
+ 
+function apagar(id){
+    let sql = "Delete from users where id = ?"
+    connection.query(sql, id, function(error, results, fields){
+        if(error) throw error
+        console.log("Apagado com sucesso! " + id)
+})
+}
+ 
 connection.end()
